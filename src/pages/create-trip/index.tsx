f611e6c9647f83701a660ff2850c87de371a3338
femelo22 <luiz123jfmg@gmail.com>
2024-07-10 21:01:05 -0300

    feat: Aula 1 feita, aula 2 acabando

---
+++ src/pages/create-trip/index.tsx
@@ -0,0 +1,125 @@
+import { ArrowRight, UserRoundPlus } from 'lucide-react';
+import { FormEvent, useState } from 'react';
+import { useNavigate } from 'react-router-dom';
+import { InviteGuestsModal } from './invite-guests-modal';
+import { ConfirmTripModal } from './confirm-trip-modal';
+import { DestinationAndDateStep } from './steps/destination-and-date-step';
+import { InviteGuestsStep } from './steps/invite-guests-step';
+
+export function CreateTripPage() {
+
+  const navigate = useNavigate();
+
+  const [isGuestsInputOpen, setIsGuestsInputOpen] = useState(false);
+  const [isGuestsModalOpen, setIsGuestsModalOpen] = useState(false);
+  const [isConfirmTripModalOpen, setIsConfirmTripModalOpen] = useState(false);
+  const [emailsToInvite, setEmailsToInvite] = useState(['']);
+
+  function openGuestsInput() {
+    setIsGuestsInputOpen(true);
+  }
+
+  function closeGuestsInput() {
+    setIsGuestsInputOpen(false);
+  }
+
+  function openGuestsModal() {
+    setIsGuestsModalOpen(true);
+  }
+
+  function closeGuestsModal() {
+    setIsGuestsModalOpen(false);
+  }
+
+  function openConfirmTripModal() {
+    setIsConfirmTripModalOpen(true);
+  }
+
+  function closeConfirmTripModal() {
+    setIsConfirmTripModalOpen(false);
+  }
+
+  function addNewEmailToInvite(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault(); //Não fazer o redirecionamento do usuário direto (que é um comportamento padrão do Form)
+    const data = new FormData(event.currentTarget);
+    const email = data.get('email')?.toString();
+
+    if (!email) {
+      return
+    }
+
+    if (emailsToInvite.includes(email)) {
+      return
+    }
+
+    setEmailsToInvite([
+      ...emailsToInvite,
+      email
+    ])
+
+    event.currentTarget.reset();
+  }
+
+  function removeEmailsFromInvites(emailToRemove: string) {
+    const newEmailList = emailsToInvite.filter(email => email !== emailToRemove)
+    setEmailsToInvite(newEmailList)
+  }
+
+  function createTrip(event: FormEvent<HTMLFormElement>) {
+    event.preventDefault(); //Não fazer o redirecionamento do usuário direto (que é um comportamento padrão do Form)
+    const data = new FormData(event.currentTarget);
+
+
+    navigate('/trips/123')
+  }
+
+  return (
+    // h-screen flex items-center justify-center bg-pattern -- vai centralizar todo o conteudo na tela (bg-pattern foi configurado no tailwind.config.js)
+    // max-w-3xl px-6 text-center -- largura maxima do conteudo e texto alinhado
+    <div className="h-screen flex items-center justify-center bg-pattern">
+      <div className="max-w-3xl w-full px-6 text-center space-y-10">
+        <div className="flex flex-col items-center gap-3">
+          <img src="/logo.svg" alt="plann.er" />
+          <p>Convide seus amigos e planeje sua próxima viagem!</p>
+        </div>
+
+        <div className="space-y-4">
+          <DestinationAndDateStep
+            closeGuestsInput={closeGuestsInput}
+            isGuestsInputOpen={isGuestsInputOpen}
+            openGuestsInput={openGuestsInput}
+          />
+
+          {isGuestsInputOpen && (
+            <InviteGuestsStep
+              openGuestsModal={openGuestsModal}
+              emailsToInvite={emailsToInvite}
+              openConfirmTripModal={openConfirmTripModal}
+            />
+          )}
+        </div>
+
+        <p className="text-sm text-zinc-500">Ao planejar sua viagem pela plann.er você automaticamente concorda <br />
+          com nossos <a href="#" className="text-zinc-300 underline">termos de uso</a> e <a href="#" className="text-zinc-300 underline">políticas de privacidade.</a>
+        </p>
+      </div>
+
+
+      {isGuestsModalOpen && (
+        <InviteGuestsModal
+          closeGuestsModal={closeGuestsModal}
+          emailsToInvite={emailsToInvite}
+          addNewEmailToInvite={addNewEmailToInvite}
+          removeEmailsFromInvites={removeEmailsFromInvites}
+        />
+      )}
+
+      {isConfirmTripModalOpen && (
+        <ConfirmTripModal
+          closeConfirmTripModal={closeConfirmTripModal}
+          createTrip={createTrip}
+        />
+      )}
+    </div>
+  )
+}
