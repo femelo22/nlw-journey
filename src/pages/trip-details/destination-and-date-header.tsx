7d1afcea9ab7c9ca29b292e6be82a0ac41107e17
femelo22 <luiz123jfmg@gmail.com>
2024-07-10 22:22:17 -0300

    refac: criando componentes trip-details

---
+++ src/pages/trip-details/destination-and-date-header.tsx
@@ -0,0 +1,30 @@
+import { Calendar, MapPin, Settings2 } from "lucide-react";
+import { Button } from "../../components/button";
+
+export function DestinationAndDateHeader() {
+  return (
+    <div className="px-4 h-16 rounded-xl bg-zinc-900 shadow-shape flex items-center justify-between">
+      <div className="flex items-center gap-2">
+        <MapPin className="size-5 text-zinc-400" />
+        <span className="text-zinc-100">Florianópolis, Brasil</span>
+      </div>
+
+
+      <div className="flex items-center gap-5">
+        <div className="flex items-center gap-2">
+          <Calendar className="size-5 text-zinc-400" />
+          <span className="text-zinc-100">17 a 23 de agosto</span>
+        </div>
+
+        <div className="w-px h-6 bg-zinc-800" />
+
+        {/* Maneira de enviar um conteúdo para algum componente 
+            O componte que receberá, tera uma propriedade children em sua interface*/}
+        <Button onClick={() => alert('teste')}>
+          Alterar local/data
+          <Settings2 className="size-5" />
+        </Button>
+      </div>
+    </div>
+  )
+}
